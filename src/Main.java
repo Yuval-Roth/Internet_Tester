6d05a27d4cfae7f39b74bbef0aa9ee3960fa4f93
Yuval Roth <rothyuv@post.bgu.ac.il>
2023-12-03 20:00:43 +0200

    version 2
    - simplified log messages. instead of spamming the seconds that were offline, it now prints time of disconnection and time of reconnection
    
    - added arguments for address(first arg) and timeout(second arg, if not entered, will use default values)
    
    - more updates to log messages. moved time stamp to the beginning of the message
    
    - improved connection detection to (hopefully) remove false disconnections and made the time of disconnection more accurate
    
    - improved file logging system, made each address being logged to create a separate file. allows for multiple logs running at the same time. Also, the folder path is no longer hard coded.
    
    - added a monitoring animation

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,102 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Main{
+public class Main {
 
-    public static void main(String[] args)
-            throws IOException
-    {
+    static String address;
+    private static char symbol = '|';
+    private static Thread animator = new Thread(Main::animateMonitoring);
+    private static AtomicBoolean shouldAnimate;
+
+    public static void main(String[] args) throws IOException {
+        address = args[0];
+
+        // print start message
         LocalDateTime now = LocalDateTime.now();
         String timestamp = getTimestamp(now);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Yuval\\Desktop\\internet logs\\internet log.txt", true))) {
-            writer.write("\nStarted logging at "+timestamp+"\n");
+        String message = "\n[%s] Started logging connection to \"%s\"%s\n".formatted(timestamp, args[0], args.length > 1 ? " with timeout of " + args[1] : "");
+        System.out.print(message);
+        try (BufferedWriter writer = getWriter()) {
+            writer.write(message);
         }
-        System.out.println("Started logging at "+timestamp+"\n");
+
         // create the ping command as a list of strings
-        Main ping = new Main();
         List<String> commands = new ArrayList<String>();
         commands.add("ping");
+        commands.add(address);
+        if(args.length > 1){
+            commands.add("-w");
+            commands.add(args[1]);
+        }
         commands.add("-n");
         commands.add("1");
-        commands.add("-w");
-        commands.add("1000");
-        commands.add("www.google.com");
-        while (true){
-            if(ping.doCommand(commands)){
-                try{
-                    Thread.sleep(1000);
-                } catch(InterruptedException ignored){}
-            }
+        commands.add("-4");
+
+        // start
+        shouldAnimate = new AtomicBoolean(true);
+        animator.start();
+        while(true){
+            testConnection(commands);
+            try{
+              Thread.sleep(1000);
+            } catch(InterruptedException ignored){}
         }
+    }
+
+    private static void testConnection(List<String> commands) throws IOException {
+
+        LocalDateTime now = LocalDateTime.now();
+        String timestamp = getTimestamp(now);
+        if (!doCommand(commands)){
+            int failCounter = 1;
+            while(!doCommand(commands)){
+                failCounter++;
+                if(failCounter == 2){
+                    try {
+                        shouldAnimate.set(false);
+                        animator.join();
+                    } catch (InterruptedException ignored) {}
+                    String message = "[%s] Lost connection\n".formatted(timestamp);
+                    System.out.print(message);
+                    try (BufferedWriter writer = getWriter()) {
+                        writer.write(message);
+                    }
+                    shouldAnimate.set(true);
+                    animator.start();
+                }
+            }
+            if(failCounter < 2) return;
+
+            try {
+                shouldAnimate.set(false);
+                animator.join();
+            } catch (InterruptedException ignored) {}
+
+            now = LocalDateTime.now();
+            timestamp = getTimestamp(now);
+            String message = "[%s] Found connection\n".formatted(timestamp);
+            System.out.print(message);
+            try (BufferedWriter writer = getWriter()) {
+                writer.write(message);
+            }
+            shouldAnimate.set(true);
+            animator.start();
+        }
+    }
+
+    private static BufferedWriter getWriter() throws IOException {
+        String folderPath = Main.class.getResource("Main.class").getPath();
+        folderPath = folderPath.replace("%20"," "); //fix space character
+        folderPath = folderPath.substring(folderPath.indexOf("/")+1); // remove initial '/'
+        folderPath = folderPath.substring(0,folderPath.lastIndexOf("/")); // remove .class file from path
+        folderPath = folderPath.substring(0,folderPath.lastIndexOf("/")+1); // exit jar
+        folderPath = folderPath.replace("/","\\");
+        String logName = "internet log - "+address+".txt";
+
+        return new BufferedWriter(new FileWriter(folderPath+logName, true));
     }
 
     private static String getTimestamp(LocalDateTime time) {
@@ -40,10 +104,10 @@ public class Main{
                 + fixNumber(time.getHour()) + ":" + fixNumber(time.getMinute()) + ":" + fixNumber(time.getSecond());
     }
 
-    public boolean doCommand(List<String> command)
+    private static boolean doCommand(List<String> command)
             throws IOException
     {
-        String s = null;
+        String s;
 
         ProcessBuilder pb = new ProcessBuilder(command);
         Process process = pb.start();
@@ -51,21 +115,12 @@ public class Main{
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
         while ((s = stdInput.readLine()) != null) {
-            if (notConnected(s)) {
-                LocalDateTime now = LocalDateTime.now();
-                String timestamp = getTimestamp(now);
-                System.out.println("No internet connection at time: " + timestamp);
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\Yuval\\Desktop\\internet logs\\internet log.txt", true))) {
-                    writer.write("No internet connection at time: " + timestamp + "\n");
-                }
-                if (s.contains("could not find host")) return true;
-                return false;
-            }
+            if (notConnected(s)) return false;
         }
         return true;
     }
 
-    public static boolean notConnected(String s) {
+    private static boolean notConnected(String s) {
         s = s.toLowerCase();
         String[] keyWords = {
                 "100% loss",
@@ -80,11 +135,30 @@ public class Main{
         return false;
     }
 
-    public static String fixNumber(int num){
-        return num < 10 ? "0"+num : ""+num;
+    private static char getNextSymbol(char c){
+        return switch(c){
+            case '|' -> '/';
+            case '/' -> '-';
+            case '-' -> '\\';
+            case '\\' -> '|';
+            default -> throw new RuntimeException("Illegal Symbol");
+        };
     }
 
+    private static void animateMonitoring() {
+        while(shouldAnimate.get()){
+            System.out.print("\rMonitoring [ "+ symbol+" ]");
+            symbol = getNextSymbol(symbol);
+            try{
+                Thread.sleep(250);
+            } catch(InterruptedException ignored){}
+        }
+        System.out.print("\r");
+        animator = new Thread(Main::animateMonitoring);
+    }
+
+
+    private static String fixNumber(int num){
+        return num < 10 ? "0"+num : ""+num;
+    }
 }
-
-
-
