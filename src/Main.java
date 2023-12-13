f58e1fd8fbf06009272617b316d6447bcff8a3de
Yuval Roth <rothyuv@post.bgu.ac.il>
2023-12-13 14:49:44 +0200

    version 3
    - Added ping sounds when disconnecting / connecting
    
    - Added a config file instead of command line arguments. now the only command line argument is the target address
    
    - attempt to improve the connection testing. changed from checking if 2000ms fails twice to 4000ms fails once. Also more frequest tests with lower waiting time for when the internet returns
    
    - Added an option to disable the pings by putting 0 in the ping count (or any number lower than 1)

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,48 @@
+import javax.sound.sampled.*;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
+    public static final String DEFAULT_CONFIG_FILE = """
+            timeout: 4000
+            master_gain: -24.0
+            disconnect_ping_count: 1
+            connect_ping_count: 1
+            """;
     static String address;
     private static char symbol = '|';
+    private static Clip clip;
     private static Thread animator = new Thread(Main::animateMonitoring);
     private static AtomicBoolean shouldAnimate;
+    private static Map<String, String> config;
+    private static Integer timeout;
+    private static Integer disconnect_ping_count;
+    private static float master_gain;
+    private static int connect_ping_count;
 
     public static void main(String[] args) throws IOException {
+
         address = args[0];
+
+        //read from config or default
+        config = readConfig();
+        timeout = config.get("timeout") == null ? 4000 : Integer.parseInt(config.get("timeout"));
+        disconnect_ping_count = config.get("disconnect_ping_count") == null ? 1 : Integer.parseInt(config.get("disconnect_ping_count"));
+        connect_ping_count = config.get("connect_ping_count") == null ? 1 : Integer.parseInt(config.get("connect_ping_count"));
+        master_gain = config.get("master_gain") == null ? -24.0f : Float.parseFloat(config.get("master_gain"));
 
         // print start message
         LocalDateTime now = LocalDateTime.now();
         String timestamp = getTimestamp(now);
-        String message = "\n[%s] Started logging connection to \"%s\"%s\n".formatted(timestamp, args[0], args.length > 1 ? " with timeout of " + args[1] : "");
+        String message = "\n[%s] Started logging connection to \"%s\" with timeout of %s\n".formatted(timestamp, args[0],timeout);
         System.out.print(message);
-        try (BufferedWriter writer = getWriter()) {
+        try (BufferedWriter writer = getLogWriter()) {
             writer.write(message);
         }
 
@@ -27,13 +50,16 @@ public class Main {
         List<String> commands = new ArrayList<String>();
         commands.add("ping");
         commands.add(address);
-        if(args.length > 1){
-            commands.add("-w");
-            commands.add(args[1]);
-        }
+        commands.add("-w");
+        commands.add(timeout.toString());
         commands.add("-n");
         commands.add("1");
-        commands.add("-4");
+
+        try {
+            clip = AudioSystem.getClip();
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException(e);
+        }
 
         // start
         shouldAnimate = new AtomicBoolean(true);
@@ -46,57 +72,93 @@ public class Main {
         }
     }
 
+    private static Map<String,String> readConfig() throws IOException {
+
+        Map<String,String> config = new HashMap<>();
+
+        try (BufferedReader configFile = new BufferedReader(new FileReader(getFolderPath()+"config.txt"))) {
+            String line;
+            while ((line = configFile.readLine()) != null && line.contains(":")){
+                String key = line.substring(0,line.indexOf(":")).strip().toLowerCase();
+                String value = line.substring((line.indexOf(":")+1)).strip().toLowerCase();
+                config.put(key,value);
+            }
+        } catch (FileNotFoundException e){
+            try(BufferedWriter writer = getWriter("config.txt")){
+                writer.write(DEFAULT_CONFIG_FILE);
+            };
+            return readConfig();
+        }
+        return config;
+    }
+
     private static void testConnection(List<String> commands) throws IOException {
 
         LocalDateTime now = LocalDateTime.now();
         String timestamp = getTimestamp(now);
         if (!doCommand(commands)){
-            int failCounter = 1;
-            while(!doCommand(commands)){
-                failCounter++;
-                if(failCounter == 2){
-                    try {
-                        shouldAnimate.set(false);
-                        animator.join();
-                    } catch (InterruptedException ignored) {}
-                    String message = "[%s] Lost connection\n".formatted(timestamp);
-                    System.out.print(message);
-                    try (BufferedWriter writer = getWriter()) {
-                        writer.write(message);
-                    }
-                    shouldAnimate.set(true);
-                    animator.start();
-                }
-            }
-            if(failCounter < 2) return;
 
-            try {
-                shouldAnimate.set(false);
-                animator.join();
-            } catch (InterruptedException ignored) {}
-
-            now = LocalDateTime.now();
-            timestamp = getTimestamp(now);
-            String message = "[%s] Found connection\n".formatted(timestamp);
+            //log the disconnection
+            stopAnimation();
+            String message = "[%s] Lost connection\n".formatted(timestamp);
             System.out.print(message);
-            try (BufferedWriter writer = getWriter()) {
+            try (BufferedWriter writer = getLogWriter()) {
                 writer.write(message);
             }
-            shouldAnimate.set(true);
-            animator.start();
+            startAnimation();
+
+            if(disconnect_ping_count > 0) playAudio("disconnect_ping.wav",disconnect_ping_count);
+
+            // wait for connection to return
+
+            List<String> waitingCommands = new ArrayList<>(commands);
+            waitingCommands.set(3,"500");
+            while(!doCommand(waitingCommands)){}
+
+            //log the reconnection
+            stopAnimation();
+            now = LocalDateTime.now();
+            timestamp = getTimestamp(now);
+            message = "[%s] Found connection\n".formatted(timestamp);
+            System.out.print(message);
+            try (BufferedWriter writer = getLogWriter()) {
+                writer.write(message);
+            }
+            startAnimation();
+
+            if(connect_ping_count > 0) playAudio("connect_ping.wav",connect_ping_count);
         }
     }
 
-    private static BufferedWriter getWriter() throws IOException {
+    private static void startAnimation() {
+        shouldAnimate.set(true);
+        animator.start();
+    }
+
+    private static void stopAnimation() {
+        try {
+            shouldAnimate.set(false);
+            animator.join();
+        } catch (InterruptedException ignored) {}
+    }
+
+    private static BufferedWriter getLogWriter() throws IOException {
+        return getWriter("internet log - " + address + ".txt");
+    }
+
+    private static BufferedWriter getWriter(String fileName) throws IOException {
+        String folderPath = getFolderPath();
+        return new BufferedWriter(new FileWriter(folderPath+fileName, true));
+    }
+
+    private static String getFolderPath() {
         String folderPath = Main.class.getResource("Main.class").getPath();
         folderPath = folderPath.replace("%20"," "); //fix space character
         folderPath = folderPath.substring(folderPath.indexOf("/")+1); // remove initial '/'
         folderPath = folderPath.substring(0,folderPath.lastIndexOf("/")); // remove .class file from path
         folderPath = folderPath.substring(0,folderPath.lastIndexOf("/")+1); // exit jar
         folderPath = folderPath.replace("/","\\");
-        String logName = "internet log - "+address+".txt";
-
-        return new BufferedWriter(new FileWriter(folderPath+logName, true));
+        return folderPath;
     }
 
     private static String getTimestamp(LocalDateTime time) {
@@ -108,12 +170,9 @@ public class Main {
             throws IOException
     {
         String s;
-
         ProcessBuilder pb = new ProcessBuilder(command);
         Process process = pb.start();
-
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
         while ((s = stdInput.readLine()) != null) {
             if (notConnected(s)) return false;
         }
@@ -157,6 +216,21 @@ public class Main {
         animator = new Thread(Main::animateMonitoring);
     }
 
+    private static void playAudio(String fileName,int count){
+        if(clip.isOpen())
+            clip.close();
+
+        try {
+            InputStream in = Main.class.getResourceAsStream(fileName);
+            BufferedInputStream buffIn = new BufferedInputStream(in);
+            clip.open(AudioSystem.getAudioInputStream(buffIn));
+            FloatControl control = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            control.setValue(master_gain);
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+            throw new RuntimeException(e);
+        }
+        clip.loop(count-1);
+    }
 
     private static String fixNumber(int num){
         return num < 10 ? "0"+num : ""+num;
