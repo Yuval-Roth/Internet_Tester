7e512f71376f44ea4e9d0cbdd2e6a3cd23262dc7
Yuval Roth <rothyuv@post.bgu.ac.il>
2023-12-14 04:55:05 +0200

    version 4
    - added the option to ping to multiple addresses at once. There is a disconnection iff they are all disconnected. Each command line argument is a different ping target
    
    - cleaned up some code, broke up the main function into different smaller functions, added a loop that catches errors and logs them
    
    - optimized animator thread to wait() instead of dying every time
    
    - added connection indicator for every target ip

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
+
 import javax.sound.sampled.*;
 import java.io.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
@@ -15,66 +14,156 @@ public class Main {
             disconnect_ping_count: 1
             connect_ping_count: 1
             """;
-    static String address;
+    static String[] addresses;
     private static char symbol = '|';
     private static Clip clip;
     private static Thread animator = new Thread(Main::animateMonitoring);
     private static AtomicBoolean shouldAnimate;
+    private static AtomicBoolean animating;
     private static Map<String, String> config;
     private static Integer timeout;
     private static Integer disconnect_ping_count;
     private static float master_gain;
     private static int connect_ping_count;
+    private static Thread[] workerThreads;
+    private static LocalDateTime[] disconnection_time;
+    private static AtomicInteger counter;
+    private static AtomicBoolean running;
+
+    private static final char CHECK_MARK = '\u2714';
+    private static final char X_MARK = '\u274c';
 
     public static void main(String[] args) throws IOException {
 
-        address = args[0];
-
-        //read from config or default
-        config = readConfig();
-        timeout = config.get("timeout") == null ? 4000 : Integer.parseInt(config.get("timeout"));
-        disconnect_ping_count = config.get("disconnect_ping_count") == null ? 1 : Integer.parseInt(config.get("disconnect_ping_count"));
-        connect_ping_count = config.get("connect_ping_count") == null ? 1 : Integer.parseInt(config.get("connect_ping_count"));
-        master_gain = config.get("master_gain") == null ? -24.0f : Float.parseFloat(config.get("master_gain"));
-
-        // print start message
-        LocalDateTime now = LocalDateTime.now();
-        String timestamp = getTimestamp(now);
-        String message = "\n[%s] Started logging connection to \"%s\" with timeout of %s\n".formatted(timestamp, args[0],timeout);
-        System.out.print(message);
-        try (BufferedWriter writer = getLogWriter()) {
-            writer.write(message);
-        }
-
-        // create the ping command as a list of strings
-        List<String> commands = new ArrayList<String>();
-        commands.add("ping");
-        commands.add(address);
-        commands.add("-w");
-        commands.add(timeout.toString());
-        commands.add("-n");
-        commands.add("1");
-
-        try {
-            clip = AudioSystem.getClip();
-        } catch (LineUnavailableException e) {
-            throw new RuntimeException(e);
-        }
-
-        // start
-        shouldAnimate = new AtomicBoolean(true);
-        animator.start();
         while(true){
-            testConnection(commands);
             try{
-              Thread.sleep(1000);
+                // ==== initialize basic fields =====
+                workerThreads = new Thread[args.length];
+                disconnection_time = new LocalDateTime[args.length];
+                addresses = args;
+                try {
+                    clip = AudioSystem.getClip();
+                } catch (LineUnavailableException e) {
+                    throw new RuntimeException(e);
+                }
+                shouldAnimate = new AtomicBoolean(true);
+                counter = new AtomicInteger(0);
+                running = new AtomicBoolean(true);
+                animating = new AtomicBoolean(false);
+                // ==================================
+
+                readConfig();
+                initGlobalVariables();
+                initWorkerThreads();
+
+                // print start message
+                String timestamp = getTimestamp(LocalDateTime.now());
+                String message = "\n[%s] Started logging connection to %s with timeout of %s\n".formatted(timestamp, getAddressesStamp() ,timeout);
+                System.out.print(message);
+                try (BufferedWriter writer = getLogWriter()) {
+                    writer.write(message);
+                }
+
+                // start
+                animator.start();
+                for(Thread t : workerThreads) t.start();
+                mainLoop();
+
+            } catch (Exception e){
+                String timestamp = getTimestamp(LocalDateTime.now());
+                String message = timestamp+": "+ e +"\n";
+                try(BufferedWriter writer = getWriter("error-log")){
+                    writer.write(message);
+                }
+                System.out.println(message);
+                e.printStackTrace();
+                System.out.println("Restarting...\n\n\n\n");
+            }
+        }
+    }
+
+    private static void mainLoop() {
+        while(true){
+            try {
+                checkIfDisconnected();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try{
+                Thread.sleep(100);
             } catch(InterruptedException ignored){}
         }
     }
 
-    private static Map<String,String> readConfig() throws IOException {
+    private static void initWorkerThreads() {
+        for(int i = 0; i < addresses.length; i++){
+            // create the ping command as a list of strings
+            List<String> commands = new ArrayList<String>();
+            commands.add("ping");
+            commands.add(addresses[i]);
+            commands.add("-w");
+            commands.add((timeout)+"");
+            commands.add("-n");
+            commands.add("1");
+            int threadIndex = i;
+            workerThreads[i] = new Thread(()->{
+                workerThreadMainLoop(commands, threadIndex);
+            });
+        }
+    }
 
-        Map<String,String> config = new HashMap<>();
+    private static void workerThreadMainLoop(List<String> commands, int threadIndex) {
+        while(running.get()){
+            try{
+                LocalDateTime now = LocalDateTime.now();
+                if (!doCommand(commands)){
+
+                    disconnection_time[threadIndex] = now;
+                    int expected;
+                    int _new;
+                    do {
+                        expected = counter.get();
+                        _new = expected+1;
+                    } while(! counter.compareAndSet(expected,_new));
+
+                    // wait for connection to return
+                    List<String> waitingCommands = new ArrayList<>(commands);
+                    waitingCommands.set(3,"500");
+                    while(!doCommand(commands)){
+                        try{
+                            Thread.sleep(100);
+                        } catch (InterruptedException ignored) {}
+                    }
+                    do {
+                        expected = counter.get();
+                        _new = expected-1;
+                    } while(! counter.compareAndSet(expected,_new));
+                    synchronized (disconnection_time[threadIndex]){
+                        disconnection_time[threadIndex] = null;
+                    }
+                }
+            } catch (IOException e){
+                throw new RuntimeException(e);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {}
+        }
+    }
+
+
+    private static String getAddressesStamp() {
+        StringBuilder builder = new StringBuilder();
+        for (String address : addresses) {
+            builder.append("\"").append(address).append("\"").append(",");
+        }
+        builder.deleteCharAt(builder.length()-1);
+        return builder.toString();
+    }
+
+    private static void readConfig() throws IOException {
+
+        config = new HashMap<>();
 
         try (BufferedReader configFile = new BufferedReader(new FileReader(getFolderPath()+"config.txt"))) {
             String line;
@@ -87,17 +176,37 @@ public class Main {
             try(BufferedWriter writer = getWriter("config.txt")){
                 writer.write(DEFAULT_CONFIG_FILE);
             };
-            return readConfig();
+            readConfig();
         }
-        return config;
     }
 
-    private static void testConnection(List<String> commands) throws IOException {
+    private static void initGlobalVariables() {
+        timeout = config.get("timeout") == null ? 4000 : Integer.parseInt(config.get("timeout"));
+        disconnect_ping_count = config.get("disconnect_ping_count") == null ? 1 : Integer.parseInt(config.get("disconnect_ping_count"));
+        connect_ping_count = config.get("connect_ping_count") == null ? 1 : Integer.parseInt(config.get("connect_ping_count"));
+        master_gain = config.get("master_gain") == null ? -24.0f : Float.parseFloat(config.get("master_gain"));
+    }
 
-        LocalDateTime now = LocalDateTime.now();
-        String timestamp = getTimestamp(now);
-        if (!doCommand(commands)){
+    private static void checkIfDisconnected() throws IOException {
 
+        if (counter.get() == addresses.length){
+
+            LocalDateTime first;
+            synchronized (disconnection_time){
+
+                // for the small chance that one of the threads resets before
+                // the main thread can log the disconnection
+                for(LocalDateTime t : disconnection_time) if(t == null) return;
+
+                first = disconnection_time[0];
+                for(int i = 1; i < addresses.length;i++){
+                    if(disconnection_time[i].isBefore(first)){
+                        first = disconnection_time[i];
+                    }
+                }
+            }
+
+            String timestamp = getTimestamp(first);
             //log the disconnection
             stopAnimation();
             String message = "[%s] Lost connection\n".formatted(timestamp);
@@ -110,14 +219,15 @@ public class Main {
             if(disconnect_ping_count > 0) playAudio("disconnect_ping.wav",disconnect_ping_count);
 
             // wait for connection to return
-
-            List<String> waitingCommands = new ArrayList<>(commands);
-            waitingCommands.set(3,"500");
-            while(!doCommand(waitingCommands)){}
+            while(counter.get() == addresses.length){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ignored) {}
+            }
 
             //log the reconnection
             stopAnimation();
-            now = LocalDateTime.now();
+            LocalDateTime now = LocalDateTime.now();
             timestamp = getTimestamp(now);
             message = "[%s] Found connection\n".formatted(timestamp);
             System.out.print(message);
@@ -132,18 +242,21 @@ public class Main {
 
     private static void startAnimation() {
         shouldAnimate.set(true);
-        animator.start();
+        animator.interrupt();
     }
 
     private static void stopAnimation() {
-        try {
-            shouldAnimate.set(false);
-            animator.join();
-        } catch (InterruptedException ignored) {}
+        shouldAnimate.set(false);
+        while(animating.get()){
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ignored) {}
+        }
     }
 
     private static BufferedWriter getLogWriter() throws IOException {
-        return getWriter("internet log - " + address + ".txt");
+        String stamp = getAddressesStamp().replace("\"","").replace(","," - ");
+        return getWriter("internet log - " + stamp + ".txt");
     }
 
     private static BufferedWriter getWriter(String fileName) throws IOException {
@@ -205,15 +318,30 @@ public class Main {
     }
 
     private static void animateMonitoring() {
-        while(shouldAnimate.get()){
-            System.out.print("\rMonitoring [ "+ symbol+" ]");
-            symbol = getNextSymbol(symbol);
-            try{
-                Thread.sleep(250);
-            } catch(InterruptedException ignored){}
+        String msg = "";
+        Object o = new Object();
+        while(running.get()){
+            animating.set(true);
+            while(shouldAnimate.get()){
+                msg = "\rMonitoring [ "+ symbol+" ]";
+                for (int i = 0; i< addresses.length;i++){
+                    msg += " - "+addresses[i]+" [ %s ]".formatted(disconnection_time[i] == null ? "OK" : "XX");
+                }
+                System.out.print(msg);
+                symbol = getNextSymbol(symbol);
+                try{
+                    Thread.sleep(250);
+                } catch(InterruptedException ignored){}
+            }
+            System.out.print(msg.replaceAll("."," "));
+            System.out.print('\r');
+            try {
+                animating.set(false);
+                synchronized (o){
+                    o.wait();
+                }
+            } catch (InterruptedException ignored) {}
         }
-        System.out.print("\r");
-        animator = new Thread(Main::animateMonitoring);
     }
 
     private static void playAudio(String fileName,int count){
